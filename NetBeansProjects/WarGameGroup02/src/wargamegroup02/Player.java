1cbcf59a38c10bca3ac155e8956bd3a5a2f25ab7
jingwenhaung <133809176+jingwenhaung@users.noreply.github.com>
2023-06-25 09:22:22 -0400

    Update Player.java

--- NetBeansProjects/WarGameGroup02/src/wargamegroup02/Player.java
+++ NetBeansProjects/WarGameGroup02/src/wargamegroup02/Player.java
@@ -1,15 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player{
     private String name;
     private int id;
-
-    public Player(){
-
-    }
+    private List<Card> hand;
+    private int scores;
 
     public Player(String name, int id){
         this.name = name;
         this.id = id;
+        this.hand = new ArrayList<>();
+        this.scores = 0;
     }
 
     public String getName() {
@@ -28,9 +30,31 @@ public class Player{
         this.id = id;
     }
 
-    @Override
-    public String toString(){
-        return this.name + " " + this.id;
+    public List<Card> getHand() {
+        return hand;
+    }
+    public void setScores (int scores){
+        this.scores = scores;
     }
 
+    public int getScores() {
+        return scores;
+    }
+
+    @Override
+    public String toString(){
+        return "name: " + this.name + "  ID: " + this.id + "  Scores: " +this.scores;
+    }
+
+    public void addCardToHand(Card card) {
+        hand.add(card);
+    }
+
+    public Card showCard() {
+        return hand.remove(0);
+    }
+
+    public boolean hasCards() {
+        return !hand.isEmpty();
+    }
 }
