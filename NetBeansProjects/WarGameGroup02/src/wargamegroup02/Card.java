5e7a0d4ab7bdfe7243725d945eca0f8e4e04c766
jingwenhaung <133809176+jingwenhaung@users.noreply.github.com>
2023-06-25 09:13:57 -0400

    Update Card.java

--- NetBeansProjects/WarGameGroup02/src/wargamegroup02/Card.java
+++ NetBeansProjects/WarGameGroup02/src/wargamegroup02/Card.java
@@ -4,9 +4,21 @@ public class Card {
         HEARTS, CLUBS, SPADES, DIAMONDS
     }
 
-    public enum Value{
-        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, 
-        EIGHT, NINE, TEN, JACK, QUEEN, KING
+    enum Value {
+        // Card values for comparison
+        ACE(14), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6),
+        SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12),
+        KING(13);
+
+        private int value;
+
+        private Value(int value) {
+            this.value = value;
+        }
+
+         public int getValue() {
+            return value;
+        }
     }
 
     private Value value;
